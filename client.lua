8e74f1dfcd63bca8997e97c96fb96509fe0ac8e7
keiki <50743267+666rejections@users.noreply.github.com>
2020-03-06 23:30:02 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -13,9 +13,9 @@ Citizen.CreateThread(function()
 
                 if ((distance < playerNamesDist) and IsEntityVisible(GetPlayerPed(id))) ~= GetPlayerPed( -1 ) then
 		    if NetworkIsPlayerTalking(id) then
-			DrawMarker(6,x2,y2,z2 - takeaway, 0, 0, 0, -90.0, 0, 0, 1.0, 1.0, 10.3, 55, 160, 205, 105, 0, 0, 2, 0, 0, 0, 0)
+			DrawMarker(6, x2, y2, z2 - takeaway, 0, 0, 0, -90.0, 0, 0, 1.0, 1.0, 10.3, 55, 160, 205, 105, 0, 0, 2, 0, 0, 0, 0)
 		    else
-			DrawMarker(6, x2,y2,z2 - takeaway, 0, 0, 0, -90.0, 0, 0, 1.0, 1.0, 0.5, 239, 239, 239, 50, 0, 0, 2, 0, 0, 0, 0)
+			DrawMarker(6, x2, y2, z2 - takeaway, 0, 0, 0, -90.0, 0, 0, 1.0, 1.0, 0.5, 239, 239, 239, 50, 0, 0, 2, 0, 0, 0, 0)
                     end
                 end  
             end
