3b84af85187676493c166bd80536c8a12cd4aac6
keiki <50743267+666rejections@users.noreply.github.com>
2020-03-06 23:29:36 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -5,7 +5,7 @@ Citizen.CreateThread(function()
         for id = 0, 31 do
             if  ((NetworkIsPlayerActive( id )) and GetPlayerPed( id ) ~= GetPlayerPed( -1 )) then
                 ped = GetPlayerPed( id )
- 
+
                 x1, y1, z1 = table.unpack( GetEntityCoords( GetPlayerPed( -1 ), true ) )
                 x2, y2, z2 = table.unpack( GetEntityCoords( GetPlayerPed( id ), true ) )
                 distance = math.floor(GetDistanceBetweenCoords(x1,  y1,  z1,  x2,  y2,  z2,  true))
@@ -13,9 +13,9 @@ Citizen.CreateThread(function()
 
                 if ((distance < playerNamesDist) and IsEntityVisible(GetPlayerPed(id))) ~= GetPlayerPed( -1 ) then
 		    if NetworkIsPlayerTalking(id) then
-			DrawMarker(25,x2,y2,z2 - takeaway, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 10.3, 55, 160, 205, 105, 0, 0, 2, 0, 0, 0, 0)
+			DrawMarker(6,x2,y2,z2 - takeaway, 0, 0, 0, -90.0, 0, 0, 1.0, 1.0, 10.3, 55, 160, 205, 105, 0, 0, 2, 0, 0, 0, 0)
 		    else
-			DrawMarker(25, x2,y2,z2 - takeaway, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 0.5, 239, 239, 239, 50, 0, 0, 2, 0, 0, 0, 0)
+			DrawMarker(6, x2,y2,z2 - takeaway, 0, 0, 0, -90.0, 0, 0, 1.0, 1.0, 0.5, 239, 239, 239, 50, 0, 0, 2, 0, 0, 0, 0)
                     end
                 end  
             end
